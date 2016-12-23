d78838b5024f5f5212a0b80527828dea247455f3
lucasbarros <lucas-barros28@hotmail.com>
2016-12-23 11:27:04 -0300

    Pedido Venda - Tela Pedido

---
+++ src/main/java/controller/CadastroPedidoBean.java
@@ -0,0 +1,24 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class CadastroPedidoBean {
+
+    private List<Integer> itens;
+
+    public CadastroPedidoBean() {
+        itens = new ArrayList<>();
+        itens.add(1);
+    }
+
+    public List<Integer> getItens() {
+        return itens;
+    }
+
+}
